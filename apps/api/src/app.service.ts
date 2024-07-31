0ed6f02b2e853e75d20ae4f47ce1be1160a113f9
H P <106737575+hp-8@users.noreply.github.com>
2024-07-31 19:00:15 +0530

    Delete apps/api/src directory

--- apps/api/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { add } from '@parkup/sample-lib'
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!' + add(23, 4)
-  }
-}
